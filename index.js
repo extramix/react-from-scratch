0e0db516e7054f74893ebe6f63685386fa19805c
ミック (Sirapat) <sirapat.jr@gmail.com>
2024-07-11 12:02:59 +0900

    First working example with createElement and render

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const createElement = (text) => {
+const createTextElement = (text) => {
   return {
     text: 'TEXT_ELEMENT',
     props: {
@@ -8,8 +8,39 @@ const createElement = (text) => {
   };
 };
 
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
+};
+
+const render = (element, container) => {
+  const dom =
+    element.text === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  //filter out non-children and assign element props to the node
+  const isProperty = (key) => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach((child) => render(child, dom));
+  container.appendChild(dom);
+};
+
 const Didact = {
   createElement,
+  render,
 };
 
 /** @jsx Didact.createElement */
@@ -19,3 +50,6 @@ const element = (
     <b />
   </div>
 );
+
+const container = document.getElementById('root');
+Didact.render(element, container);
